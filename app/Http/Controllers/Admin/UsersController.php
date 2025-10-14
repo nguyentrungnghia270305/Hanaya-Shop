6c96b2de19ad40e9e38e1babd8fc230b83a3dd04
Nguyen Trung Nghia <assassincreed2k1@gmail.com>
2025-10-14 22:54:20 +0700

    Add basic CRUD logic to Admin UsersController

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -4,7 +4,75 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 
+use App\Models\User;
+use Illuminate\Http\Request;
+
 class UsersController extends Controller
 {
-    // ...
+    // List all users
+    public function index()
+    {
+        $users = User::all();
+        return view('admin.users.index', compact('users'));
+    }
+
+    // Show user detail
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        return view('admin.users.show', compact('user'));
+    }
+
+    // Show create form
+    public function create()
+    {
+        return view('admin.users.create');
+    }
+
+    // Store new user
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+        ]);
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        return redirect()->route('admin.user.index');
+    }
+
+    // Show edit form
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        return view('admin.users.edit', compact('user'));
+    }
+
+    // Update user
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email,' . $user->id,
+        ]);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        if ($request->filled('password')) {
+            $user->password = bcrypt($request->password);
+        }
+        $user->save();
+        return redirect()->route('admin.user.index');
+    }
+
+    // Delete user
+    public function destroy($id)
+    {
+        User::destroy($id);
+        return redirect()->route('admin.user.index');
+    }
 }
