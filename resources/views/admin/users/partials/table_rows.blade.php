6cb663388ca3839f5b46f491e117939c9dc6c57a
Nguyen Trung Nghia <assassincreed2k1@gmail.com>
2025-10-20 00:15:49 +0700

    Refactor user table: responsive, accessibility, status column, Blade components

--- resources/views/admin/users/partials/table_rows.blade.php
+++ resources/views/admin/users/partials/table_rows.blade.php
@@ -1,31 +1,41 @@
 {{-- resources/views/admin/users/partials/table_rows.blade.php --}}
-<table class="table-auto w-full">
-    <thead>
-        <tr>
-            <th class="px-4 py-2">ID</th>
-            <th class="px-4 py-2">Name</th>
-            <th class="px-4 py-2">Email</th>
-            <th class="px-4 py-2">Role</th>
-            <th class="px-4 py-2">Actions</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($users as $user)
-        <tr>
-            <td class="border px-4 py-2">{{ $user->id }}</td>
-            <td class="border px-4 py-2">{{ $user->name }}</td>
-            <td class="border px-4 py-2">{{ $user->email }}</td>
-            <td class="border px-4 py-2">{{ $user->role }}</td>
-            <td class="border px-4 py-2">
-                <a href="{{ route('admin.user.show', $user->id) }}" class="text-blue-500">View</a>
-                <a href="{{ route('admin.user.edit', $user->id) }}" class="text-green-500 ml-2">Edit</a>
-                <form action="{{ route('admin.user.destroy', $user->id) }}" method="POST" style="display:inline">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="text-red-500 ml-2" onclick="return confirm('Are you sure?')">Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+<div class="overflow-x-auto">
+    <table class="min-w-full divide-y divide-gray-200">
+        <thead class="bg-gray-50">
+            <tr>
+                <th scope="col" class="px-4 py-2">ID</th>
+                <th scope="col" class="px-4 py-2">Name</th>
+                <th scope="col" class="px-4 py-2">Email</th>
+                <th scope="col" class="px-4 py-2">Role</th>
+                <th scope="col" class="px-4 py-2">Status</th>
+                <th scope="col" class="px-4 py-2">Actions</th>
+            </tr>
+        </thead>
+        <tbody class="bg-white divide-y divide-gray-200">
+            @foreach($users as $user)
+            <tr>
+                <td class="px-4 py-2">{{ $user->id }}</td>
+                <td class="px-4 py-2">{{ $user->name }}</td>
+                <td class="px-4 py-2">{{ $user->email }}</td>
+                <td class="px-4 py-2">{{ $user->role }}</td>
+                <td class="px-4 py-2">
+                    @if($user->active ?? true)
+                        <span class="text-green-600 font-semibold">Active</span>
+                    @else
+                        <span class="text-red-600 font-semibold">Inactive</span>
+                    @endif
+                </td>
+                <td class="px-4 py-2 flex gap-2">
+                    <x-button-link :href="route('admin.user.show', $user->id)" color="blue">View</x-button-link>
+                    <x-button-link :href="route('admin.user.edit', $user->id)" color="green">Edit</x-button-link>
+                    <form action="{{ route('admin.user.destroy', $user->id) }}" method="POST" style="display:inline">
+                        @csrf
+                        @method('DELETE')
+                        <x-button type="submit" color="red" onclick="return confirm('Are you sure?')">Delete</x-button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
