9f4c3d06800a32af9843ed570e557a99da591ffc
Nguyen Trung Nghia <assassincreed2k1@gmail.com>
2025-10-20 00:16:09 +0700

    Improve user table: tooltips, alternate row colors, keyboard navigation

--- resources/views/admin/users/partials/table_rows.blade.php
+++ resources/views/admin/users/partials/table_rows.blade.php
@@ -13,25 +13,25 @@
         </thead>
         <tbody class="bg-white divide-y divide-gray-200">
             @foreach($users as $user)
-            <tr>
-                <td class="px-4 py-2">{{ $user->id }}</td>
-                <td class="px-4 py-2">{{ $user->name }}</td>
-                <td class="px-4 py-2">{{ $user->email }}</td>
-                <td class="px-4 py-2">{{ $user->role }}</td>
-                <td class="px-4 py-2">
+            <tr tabindex="0" class="focus:bg-blue-50 @if($loop->even) bg-gray-100 @endif" title="User ID: {{ $user->id }}">
+                <td class="px-4 py-2" title="User ID">{{ $user->id }}</td>
+                <td class="px-4 py-2" title="Full name">{{ $user->name }}</td>
+                <td class="px-4 py-2" title="Email address">{{ $user->email }}</td>
+                <td class="px-4 py-2" title="Role">{{ $user->role }}</td>
+                <td class="px-4 py-2" title="Status">
                     @if($user->active ?? true)
-                        <span class="text-green-600 font-semibold">Active</span>
+                        <span class="text-green-600 font-semibold" title="Active">Active</span>
                     @else
-                        <span class="text-red-600 font-semibold">Inactive</span>
+                        <span class="text-red-600 font-semibold" title="Inactive">Inactive</span>
                     @endif
                 </td>
                 <td class="px-4 py-2 flex gap-2">
-                    <x-button-link :href="route('admin.user.show', $user->id)" color="blue">View</x-button-link>
-                    <x-button-link :href="route('admin.user.edit', $user->id)" color="green">Edit</x-button-link>
+                    <x-button-link :href="route('admin.user.show', $user->id)" color="blue" title="View user details">View</x-button-link>
+                    <x-button-link :href="route('admin.user.edit', $user->id)" color="green" title="Edit user">Edit</x-button-link>
                     <form action="{{ route('admin.user.destroy', $user->id) }}" method="POST" style="display:inline">
                         @csrf
                         @method('DELETE')
-                        <x-button type="submit" color="red" onclick="return confirm('Are you sure?')">Delete</x-button>
+                        <x-button type="submit" color="red" title="Delete user" onclick="return confirm('Are you sure?')">Delete</x-button>
                     </form>
                 </td>
             </tr>
