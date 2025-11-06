1537903690e35edd3846f9035fa0bb040d477cdb
Nezuko1909 <kny1909.nezuko@gail.com>
2025-11-06 09:21:14 +0700

    Create Test file

---
+++ tests/Feature/Http/Controllers/Admin/StatisticalControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Admin;
+
+use Tests\TestCase;
+use App\Models\Review;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class StatisticalControllerTest extends TestCase
+{
+    use RefreshDatabase;
+}
